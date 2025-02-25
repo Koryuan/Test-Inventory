03c08340e45024ab3709012ebcf7d7bd490e2d54
Koryuan <75565730+Koryuan@users.noreply.github.com>
2025-02-26 03:38:52 +0700

    Add search item, update and delete item controller

---
+++ Assets/Scripts/Menu/Search Item/SearchItemController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SearchItemController
+{
+    [SerializeField] private SearchItemView view;
+
+    private ItemData targetData = null;
+
+    private Action<string> ShowInformationAction;
+
+    public void Init(Action<string> ShowInformationAction)
+    {
+        this.ShowInformationAction = ShowInformationAction;
+
+        view.AddListenerToSearchButton(SearchItem);
+        view.AddListenerToUpdateButton(UpdateItem);
+        view.AddListenerToDeleteButton(DeleteItem);
+    }
+
+    private void SearchItem()
+    {
+        var searchName = view.GetSearchValue();
+        bool success = Inventory.Instance.RetriveItem(searchName, out ItemData output);
+
+        if (!success)
+        {
+            ShowInformationAction?.Invoke($"Failed to search item: {searchName}");
+            return;
+        }
+
+        targetData = output;
+
+        view.SetItemData(targetData.Name, targetData.Description);
+        view.SetSubPanelActivation(true);
+
+        ShowInformationAction?.Invoke($"Success find item name: {searchName}");
+    }
+
+    private void UpdateItem()
+    {
+        if (targetData == null)
+        {
+            ShowInformationAction("There is no data to update");
+            return;
+        }
+
+        var newDescription = view.GetDescriptionValue();
+        bool success = Inventory.Instance.UpdateItemDescription(targetData.Name, newDescription);
+        if (!success) ShowInformationAction?.Invoke($"Failed to update item");
+        else ShowInformationAction?.Invoke($"Successfully update item data");
+
+    }
+
+    private void DeleteItem()
+    {
+        if (targetData == null)
+        {
+            ShowInformationAction("There is no data to delete");
+            return;
+        }
+
+        bool success = Inventory.Instance.DeleteItem(targetData.Name);
+        if (!success) ShowInformationAction?.Invoke($"Failed to delete item");
+        else
+        {
+            view.SetSubPanelActivation(false);
+            ShowInformationAction?.Invoke($"Successfully delete item");
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/Search Item/SearchItemView.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class SearchItemView : IPanelView
+{
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject subPanel;
+    [SerializeField] private TMP_Text nameText;
+    [SerializeField] private TMP_InputField searchInputField;
+    [SerializeField] private TMP_InputField descriptionInputField;
+
+    [Header("Button")]
+    [SerializeField] private Button searchButton;
+    [SerializeField] private Button updateButton;
+    [SerializeField] private Button deleteButton;
+
+    public void SetPanelActivation(bool activation)
+    {
+        mainPanel.SetActive(activation);
+    }
+
+    public void SetSubPanelActivation(bool actvation)
+    {
+        subPanel.SetActive(actvation);
+    }
+
+    public void SetItemData(string Name, string Description)
+    {
+        nameText.text = Name;
+        descriptionInputField.text = Description;
+    }
+
+    public void ResetInputField()
+    {
+        searchInputField.text = string.Empty;
+        descriptionInputField.text = string.Empty;
+    }
+
+    public void AddListenerToSearchButton(Action OnCreate)
+    {
+        searchButton.onClick.RemoveAllListeners();
+        searchButton.onClick.AddListener(() => OnCreate?.Invoke());
+    }
+
+    public void AddListenerToUpdateButton(Action OnCreate)
+    {
+        updateButton.onClick.RemoveAllListeners();
+        updateButton.onClick.AddListener(() => OnCreate?.Invoke());
+    }
+
+    public void AddListenerToDeleteButton(Action OnCreate)
+    {
+        deleteButton.onClick.RemoveAllListeners();
+        deleteButton.onClick.AddListener(() => OnCreate?.Invoke());
+    }
+
+    public string GetSearchValue()
+        => searchInputField.text;
+
+    public string GetDescriptionValue()
+        => descriptionInputField.text;
+}
